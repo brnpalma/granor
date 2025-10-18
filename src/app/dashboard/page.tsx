8ad321e0ac2dc39f0624e4ba076b96d70af7b069
Bruno Palma Oliveira <brnoliveira23@gmail.com>
2025-10-18 19:11:38 +0000

    remova essa linha divisoria entre as contas no card contas do painel

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -563,7 +563,7 @@ export default function DashboardPage() {
                         const balance = accountBalances.get(account.id) ?? 0;
                         const forecast = accountForecasts.get(account.id) ?? 0;
                         return (
-                            <div key={account.id} className={cn("flex items-center p-3", accounts.length -1 !== index && "border-b")}>
+                            <div key={account.id} className={cn("flex items-center p-3")}>
                                 <div className="flex items-center gap-4 flex-1">
                                     <div className={cn("flex-shrink-0", account.ignoreInTotals && "opacity-50")}>
                                         <BankIcon name={account.name} color={account.color} />
@@ -722,3 +722,5 @@ export default function DashboardPage() {
   );
 
 }
+
+    
