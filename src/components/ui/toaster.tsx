b284de94724dd62070313f9bf38a7f6fd1316fb7
Bruno Palma Oliveira <brnoliveira23@gmail.com>
2025-10-18 16:44:03 +0000

    remova o esmaecer da tela quando o toast aparece e faça o toast sempre a

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -17,12 +17,6 @@ export function Toaster() {
 
   return (
     <ToastProvider>
-      <div
-        className={cn(
-          "fixed inset-0 z-40 bg-black/50 transition-opacity",
-          toasts.length > 0 ? "opacity-100" : "opacity-0 pointer-events-none"
-        )}
-      />
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast
