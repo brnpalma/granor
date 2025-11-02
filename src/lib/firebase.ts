4ec9bc69a0f0af711736de1e33318cc9bf03957c
Bruno Oliveira <br88@outlook.com>
2025-11-01 21:20:33 -0300

    teste voltando variaveis de ambiente ao que eram no firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,14 +7,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
+const firebaseConfig = {
+  apiKey: "AIzaSyCZD0UT-1BSboulpZUOQikZFakRGgPBnFQ",
+  authDomain: "granor-financas.firebaseapp.com",
+  projectId: "granor-financas",
+  storageBucket: "granor-financas.firebasestorage.app",
+  messagingSenderId: "321962799532",
+  appId: "1:321962799532:web:9f4fcd61d62c6df7848a09",
+  measurementId: "G-QWBLRJS6KT"
 };
 
 
